96141b430bd2158eee2313fd474029aa9fcdd0b3
Manidhar Sreeramakavacham <manidharm066@gmail.com>
2026-05-30 12:09:14 +0530

    Updated the CRUD operation in SQL

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 
@@ -27,6 +27,77 @@ def home():
 def get_products(db: Session = Depends(get_db)):
     products = db.query(Product).all()
     return products
+
+@app.get("/products/{product_id}")
+def get_product(
+    product_id: int,
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).filter(
+        Product.id == product_id
+    ).first()
+
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail="Product not found"
+        )
+    return product
+
+@app.get("/products/{product_id}")
+def update_product(
+    product_id: int,
+    updated_product: ProductCreate,
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).filter(
+        Product.id == product_id
+    ).first()
+
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail="Product not found"
+        )
+
+    # Update product details
+    product.name = updated_product.name
+    product.quantity = updated_product.quantity
+    product.price = updated_product.price
+
+    db.commit()
+    db.refresh(product)
+
+    return {
+        "message": "Product updated successfully", "product": {
+            "id": product.id,
+            "name": product.name,
+            "quantity": product.quantity,
+            "price": product.price
+        }
+    }
+
+@app.delete("/products/{product_id}")
+def delete_product(
+    product_id: int,
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).filter(
+        Product.id == product_id
+    ).first()
+
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail="Product not found"
+        )
+    
+    db.delete(product)
+    db.commit()
+
+    return {
+        "message": "Product deleted successfully"
+    }
 
 # Add Product
 @app.post("/products")
